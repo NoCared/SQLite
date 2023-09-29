173f06ca494975bb59cac2e4884dbb36637b4331
nocared <paul.richarddu30@gmail.com>
2023-09-29 09:38:52 +0200

    ADD - Table livre + datas

--- bibliotheque/data.sql
+++ bibliotheque/data.sql
@@ -12,7 +12,15 @@ INSERT INTO abonne (prenom_abonne) VALUES
 
 CREATE TABLE IF NOT EXISTS livre
 (
-    id_livre INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_livre INTEGER PRIMARY KEY,
     auteur_livre VARCHAR(30) NOT NULL,
     titre_livre VARCHAR(30) NOT NULL
 );
+
+INSERT INTO livre (id_livre,auteur_livre,titre_livre) VALUES
+(100,'Guy de Maupassant','Une vie'),
+(101,'Guy de Maupassant','Bel-Ami'),
+(102,'Honoré de Balzac','Le Pere Goriot'),
+(103,'Alphonse Daudet','Le petit chose'),
+(104,'Alexandre Dumas','La reine Margot'),
+(105,'Alexandre Dumas','Les Trois Mousquetaires');
